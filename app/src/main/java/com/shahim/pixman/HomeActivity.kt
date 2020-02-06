e61905152ec69015b8ef181b656ba48c7bc32907
Mohammed Shahim A <shahim.calicut@gmail.com>
2020-02-06 23:43:22 +0530

    add UI

--- app/src/main/java/com/shahim/pixman/HomeActivity.kt
+++ app/src/main/java/com/shahim/pixman/HomeActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.shahim.pixman.fragment.IntroFragment
 
 import kotlinx.android.synthetic.main.activity_home.*
 
@@ -20,6 +21,11 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         setSupportActionBar(app_bar)
+        init()
+    }
+
+    private fun init() {
+        supportFragmentManager.beginTransaction().add(R.id.container, IntroFragment()).commit()
     }
 }
 
