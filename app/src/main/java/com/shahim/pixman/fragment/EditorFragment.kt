eebd3217add27fd4439a36d4d9ba20e2ff08a43f
Mohammed Shahim A <shahim.calicut@gmail.com>
2020-02-07 02:50:28 +0530

    fix image rotation from EXIF and load bitmap

--- app/src/main/java/com/shahim/pixman/fragment/EditorFragment.kt
+++ app/src/main/java/com/shahim/pixman/fragment/EditorFragment.kt
@@ -1,8 +1,13 @@
 package com.shahim.pixman.fragment
 
-import android.R.attr.name
+import android.content.Context
+import android.database.Cursor
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
 import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -41,6 +46,8 @@ class EditorFragment : Fragment() {
 
     lateinit var imageUri: Uri
 
+    lateinit var ogImage : Bitmap
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         readBundle(arguments)
@@ -54,7 +61,54 @@ class EditorFragment : Fragment() {
     }
 
     private fun init() {
-        patient.setImageURI(imageUri)
+//        patient.setImageURI(imageUri)
+
+        val ims = activity?.contentResolver?.openInputStream(imageUri)
+        ogImage = BitmapFactory.decodeStream(ims)
+        context?.let {
+            val rtImage = rotateViaMatrix(ogImage,getOrientation(it,imageUri))
+            rtImage?.let { ogImage = rtImage }
+        }
+        ims?.close()
+
+        patient.setImageBitmap(ogImage)
+
+        tool_flip_h.setOnClickListener { flipH() }
+
+        tool_text_add.setOnClickListener { addTextLogo() }
+    }
+
+    private fun flipH() {
+
+    }
+
+    private fun addTextLogo() {
+
+    }
+
+    fun getOrientation(context: Context, photoUri: Uri): Int {
+        val cursor: Cursor? = context.contentResolver.query(photoUri,
+            arrayOf(MediaStore.Images.ImageColumns.ORIENTATION),
+            null,
+            null,
+            null
+        )
+        if (cursor?.count != 1) {
+            return -1
+        }
+        cursor.moveToFirst()
+        val or = cursor.getInt(0)
+        cursor.close()
+        return or
+    }
+
+    private fun rotateViaMatrix(original: Bitmap, orientation: Int): Bitmap? {
+        val matrix = Matrix()
+        matrix.setRotate(orientation.toFloat())
+        return Bitmap.createBitmap(
+            original, 0, 0, original.width,
+            original.height, matrix, true
+        )
     }
 }
 
