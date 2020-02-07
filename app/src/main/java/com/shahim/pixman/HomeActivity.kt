ee721fc029f8b5b95d6c6f287783c0ac1d67a401
Mohammed Shahim A <shahim.calicut@gmail.com>
2020-02-07 06:05:16 +0530

    save to gallery

--- app/src/main/java/com/shahim/pixman/HomeActivity.kt
+++ app/src/main/java/com/shahim/pixman/HomeActivity.kt
@@ -1,17 +1,23 @@
 package com.shahim.pixman
 
+import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.snackbar.Snackbar
 import com.shahim.pixman.fragment.EditorFragment
 import com.shahim.pixman.fragment.IntroFragment
-
 import kotlinx.android.synthetic.main.activity_home.*
 
-class HomeActivity : AppCompatActivity(), IntroFragment.OnImagePickedListener {
+
+class HomeActivity : AppCompatActivity(), IntroFragment.OnImagePickedListener, EditorFragment.OnImageFinishedListener {
 
     companion object {
         fun craftIntent(c: Context): Intent {
@@ -35,9 +41,28 @@ class HomeActivity : AppCompatActivity(), IntroFragment.OnImagePickedListener {
         supportFragmentManager.beginTransaction().replace(R.id.container, EditorFragment.newInstance(image)).addToBackStack(null).commit()
     }
 
+    override fun onImageFinished(image: Uri) {
+        supportFragmentManager.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+        showIntro()
+        Snackbar.make(container,R.string.editor_save_success,Snackbar.LENGTH_LONG)
+            .setAction(R.string.editor_save_view_prompt) {
+                val openFile = Intent(Intent.ACTION_VIEW)
+                intent.setDataAndType(image, "image/*")
+                try {
+                    startActivity(openFile)
+                } catch (e: ActivityNotFoundException) {
+                    Log.i("View", "Cannot open file.")
+                }
+            }
+            .show()
+    }
+
     override fun onAttachFragment(fragment: Fragment) {
         if(fragment is IntroFragment) {
-            fragment.setOnImagePickedListener(this)
+            fragment.onImagePickedListener = this
+        }
+        else if(fragment is EditorFragment) {
+            fragment.onImageFinishedListener = this
         }
     }
 }
